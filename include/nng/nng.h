5cb2d150b00886bd6862f964aa307a6fce5e2bb8
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-03-02 19:43:58 +0000

    nng 1.7.3 mbedtls 3.5.2

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -45,7 +45,7 @@ extern "C" {
 
 #define NNG_MAJOR_VERSION 1
 #define NNG_MINOR_VERSION 7
-#define NNG_PATCH_VERSION 2
+#define NNG_PATCH_VERSION 3
 #define NNG_RELEASE_SUFFIX \
 	"" // if non-empty (i.e. "pre"), this is a pre-release
 
